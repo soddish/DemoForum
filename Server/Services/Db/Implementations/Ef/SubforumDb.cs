4b5964e5880eb39b841990e65227862614a28939
Sam Jordan <samjordan112@gmail.com>
2024-11-23 15:33:32 +0000

    implemented ISubforumStore on SubforumDb

--- Server/Services/Db/Implementations/Ef/SubforumDb.cs
+++ Server/Services/Db/Implementations/Ef/SubforumDb.cs
@@ -8,4 +8,32 @@ public class SubforumDb(DbContextOptions<SubforumDb> options) :
 {
     public DbSet<Subforum> Subforums => Set<Subforum>();
 
+    async Task<bool> ISubforumStore.Add(Subforum subforum)
+    {
+        Subforums.Add(subforum);
+        await SaveChangesAsync();
+        return true;
+    }
+
+    Task<Subforum[]> ISubforumStore.GetAll() =>
+        Subforums.ToArrayAsync();
+
+    Task<Subforum?> ISubforumStore.GetById(int subforumId) =>
+        Subforums.FindAsync(subforumId).AsTask();
+
+    async Task<bool> ISubforumStore.Update(int id, Subforum inputSubforum)
+    {
+        var subforum = await Subforums
+            .FindAsync(id);
+
+        if(subforum is null)
+            return false;
+
+        subforum.Name = inputSubforum.Name;
+        subforum.NewThreadsOpen = inputSubforum.NewThreadsOpen;
+
+        await SaveChangesAsync();
+
+        return true;
+    }
 }
